213a0e62d469fcc09bbc0c6b7428b36c27847ca0
Katy <katyprogrammer@gmail.com>
2016-12-29 05:56:45 +0800

    add vanilla version

--- lib.py
+++ lib.py
@@ -14,3 +14,21 @@ def plot_validation_history(his, fig_path):
     plt.ylabel('Loss')
     plt.savefig(fig_path, dpi=300)
     plt.show()
+
+    
+########### print download progress ############
+def print_download_progress(count, block_size, total_size):
+    """
+    Function used for printing the download progress.
+    Used as a call-back function in maybe_download_and_extract().
+    """
+
+    # percentage completion.
+    pct_complete = float(count * block_size) / total_size
+
+    # status-message. Note the \r which means the line should overwrite itself.
+    msg = "\r- Download progress: {0:.1%}".format(pct_complete)
+
+    # print the message.
+    sys.stdout.write(msg)
+    sys.stdout.flush()
